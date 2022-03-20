cbc5ccd53181e759248a9c26f55f02a6d429fe3e
Artemka <71592741+ammv@users.noreply.github.com>
2022-03-20 12:49:47 +0300

    Add files via upload
    fixes:
    - case routing
    added:
    - text on cards
    - borders for cards and roulette
    - winner text
    changed:
    - roulette position

--- ver2.py
+++ ver2.py
@@ -2,13 +2,13 @@ import pygame
 import random
 
 class Characters:
-    NAMES = {"Лес": ["ЛесЧерт1", "ЛесЧерт2"],
-        "Вода": ["ВодаЧерт1", "ВодаЧерт2"],
-        "Камни": ["КамниЧерт1", "КамниЧерт2"],
-        "Болото": ["БолотоЧерт1", "БолотоЧерт2"],
-        "Равнины": ["РавниныЧерт1", "РавниныЧерт2"],
-        "Пустыня": ["ПустыняЧерт1", "ПустыняЧерт2"],
-        "Пустошь": ["ПустошьЧерт1", "ПустошьЧерт2"]}
+    NAMES = {"Лес": ["Аурены", "Ведьмы"],   # green
+        "Озера": ["Русалки", "Амфибии"],       # blue
+        "Горы": ["Гномы", "Инженеры"],# grey
+        "Болото": ["Алхимики", "Дарклинги"], #black
+        "Равнины": ["Культисты", "Полурослики"], # коричневый
+        "Пустыня": ["Кочевники", "Факиры"],   # yellow
+        "Пустошь": ["Маги Хаоса", "Гиганты"]}
         
     COLORS = {name: color for name, color in zip(NAMES, (
         (139, 148, 32), (97, 154, 198), (204, 206, 201),
@@ -18,7 +18,12 @@ class Characters:
         
     @staticmethod
     def delete_name(name):
-        del Characters.NAMES[name]
+        for key in Characters.NAMES:
+            if name in Characters.NAMES[key]: 
+                frac = key
+                break
+                
+        del Characters.NAMES[frac]
         
     @staticmethod
     def shuffle():
@@ -48,7 +53,12 @@ class Color:
     WHITE = (255,255,255)
     YELLOW = (255,255,0)
     
+    GREEN2 = (14, 123, 53)
+    
     OTHER = (225,204,79)
+    
+    ROULETTE_BORDER = (138, 127, 142)
+    CARD_BORDER = (167, 110, 25)
     
     @staticmethod
     def randcolor():
@@ -69,6 +79,12 @@ class Game:
     
     def __init__(self):
         pygame.init()
+        pygame.font.init()
+        
+        Game.CARD_FONT = pygame.font.Font(None, 24)
+        Game.WINNER_FONT = pygame.font.Font(None, 36)
+        
+        self.winner_title = Game.WINNER_FONT.render("Ваш персонаж:", True, Color.BLACK)
         
         self.clock = pygame.time.Clock()
         self.button = None
@@ -82,12 +98,12 @@ class Game:
         self.black_squares = [
             pygame.Rect(
                 0,
-                Game.HEIGHTDIV2 - Roulette.HEIGHTDIV2, 
+                self.roulette.rect.y, 
                 Game.WIDHTDIV2 - Roulette.WIDHTDIV2, 
                 Roulette.HEIGHT), 
             pygame.Rect(
                 Game.WIDHTDIV2 + Roulette.WIDHTDIV2,
-                Game.HEIGHTDIV2 - Roulette.HEIGHTDIV2,
+                self.roulette.rect.y,
                 Game.WIDHTDIV2 - Roulette.WIDHTDIV2,
                 Roulette.HEIGHT)
         ]
@@ -96,25 +112,61 @@ class Game:
         self.all_sprites.add(self.roulette)
         self.all_sprites.add(*self.roulette.cards)
         
-    def draw_black_squares(self):
-        pygame.draw.rect(self.screen, Color.GREEN, self.black_squares[0])
-        pygame.draw.rect(self.screen, Color.YELLOW, self.black_squares[1])
+    def draw_winner(self):
+        self.screen.blit(self.winner_title, (
+            Game.WIDHTDIV2-Game.WINNER_FONT.size("Ваш персонаж:")[0]/2,
+            Game.HEIGHTDIV2-Game.WINNER_FONT.size("Ваш персонаж:")[1]/2-25))
+            
+        self.screen.blit(self.roulette.text_winner, (
+            Game.WIDHTDIV2-Game.WINNER_FONT.size(self.roulette.winner)[0]/2,
+            Game.HEIGHTDIV2-Game.WINNER_FONT.size(self.roulette.winner)[1]/2 +   
+            Game.WINNER_FONT.size("Ваш персонаж:")[1]/2 + 5))
+        
+    def draw_hiding_squares(self):
+        pygame.draw.rect(self.screen, Color.GREEN2, self.black_squares[0])
+        pygame.draw.rect(self.screen, Color.GREEN2, self.black_squares[1])
             
     def set_window(self):
         #pygame.mixer.init()  # для звука
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption("Character roulette")
         
-    def draw_all(self):
-        self.all_sprites.update()   
+    def draw_text_on_cards(self):
+        for card in self.roulette.cards:
+            self.screen.blit(card.text, card.get_text_pos())
+            
+    def draw_lines(self):
+        #center red line in roulette
+        pygame.draw.line(self.screen, (230, 10, 10), 
+            self.roulette.rect.midtop, 
+            self.roulette.rect.midbottom, 3)
+            
+        #roulette border
+        pygame.draw.lines(self.screen, Color.ROULETTE_BORDER, True, 
+            [self.roulette.rect.topleft, self.roulette.rect.topright,
+            self.roulette.rect.bottomright, self.roulette.rect.bottomleft], 3)
+            
+    def draw_cards_border(self):
+        for card in self.roulette.cards:
+            pygame.draw.lines(self.screen, Color.CARD_BORDER, True, 
+            [card.rect.topleft, card.rect.topright,
+            card.rect.bottomright, card.rect.bottomleft], 1)
         
-        self.screen.fill((14, 123, 53))
+    def draw_all(self):
+        if not self.roulette.end:
+            self.all_sprites.update()
+            self.screen.fill(Color.GREEN2)
+               
+        else:
+            self.screen.fill(Color.GREEN2)
+            self.draw_winner()
         
         self.all_sprites.draw(self.screen)
-        pygame.draw.line(self.screen, (230, 10, 10), 
-            [Game.WIDHTDIV2, Game.HEIGHTDIV2 - Roulette.HEIGHTDIV2], 
-            [Game.WIDHTDIV2, Game.HEIGHTDIV2 + Roulette.HEIGHTDIV2], 3)
-        #self.draw_black_squares()
+        self.draw_cards_border()
+        self.draw_text_on_cards()
+            
+        self.draw_hiding_squares()
+        self.draw_lines()
         
         pygame.display.flip()
             
@@ -132,17 +184,18 @@ class Roulette(pygame.sprite.Sprite):
     WIDTH = 400
     WIDHTDIV2 = WIDTH / 2
     
-    HEIGHT = 110
+    HEIGHT = 100
     HEIGHTDIV2 = HEIGHT / 2
     
-    OFFSET = 10
+    OFFSET = 5
     
     RIGHT_END = WIDTH + WIDTH/2
     START_LEFT = Game.WIDHTDIV2 - WIDHTDIV2
     
-    N = 14 #count of cards now
+    STEP = 25 + random.randint(5, 15)
+    K = 0.995
     
-    
+    N = 14 #count of cards now   
     
     def __init__(self, sc_width, sc_height):
         pygame.sprite.Sprite.__init__(self)
@@ -150,7 +203,7 @@ class Roulette(pygame.sprite.Sprite):
         self.image = pygame.Surface((self.WIDTH, self.HEIGHT))
         self.image.fill(Color.WHITE)
         self.rect = self.image.get_rect()
-        self.rect.center = (sc_width/2, sc_height/2)
+        self.rect.center = (sc_width/2, sc_height/4)
         
         Roulette.ALL_WIDTH = Card.WIDTH * Roulette.N + Roulette.OFFSET * (Roulette.N+1)
         
@@ -158,37 +211,57 @@ class Roulette(pygame.sprite.Sprite):
         
         self.cards = [Card(i, data[i][0],data[i][1]) for i in range(Roulette.N)]
         
+        self.end = False
+        
+    def get_winner_character(self):
+        for card in self.cards:
+            if card.rect.x < Game.WIDHTDIV2 < card.rect.topright[0]:
+                Characters.delete_name(card.raw_text)
+                return card.raw_text
+                
+    def stop(self):
+        self.winner = self.get_winner_character()
+        self.text_winner = Game.WINNER_FONT.render(self.winner, True, Color.BLACK)
+        self.end = True
+        
     def update(self):    
-        if Card.STEP > 0:
+        if Roulette.STEP > 1:
             for card in self.cards:
                 card._update()
-            
-            #Card.STEP -= 0.01
+            Roulette.STEP *= Roulette.K
+            Roulette.K -= random.random() / 100000
+        else:
+            self.stop()
+        
             
 class Card(pygame.sprite.Sprite):
     WIDTH = 120
     HEIGHT = 90
-    STEP = 5
     
     def __init__(self,n, text, color):
         pygame.sprite.Sprite.__init__(self)
         
         self.image = pygame.Surface((Card.WIDTH, Card.HEIGHT))
         self.image.fill(color)
-        
-        self.text = text
-        
         self.rect = self.image.get_rect()
         
-        self.rect.x = Roulette.START_LEFT + Roulette.OFFSET*(n+1) + Card.WIDTH * n
-        self.rect.y = Game.HEIGHTDIV2 - Roulette.HEIGHTDIV2 + Roulette.OFFSET
+        self.text = Game.CARD_FONT.render(text, True, Color.BLACK)
+        self.raw_text = text
+        self.text_size = Game.CARD_FONT.size(text)
+        self.text_size = self.text_size[0] / 2, self.text_size[1] / 2
         
-        self.image_x = self.rect.x - Roulette.WIDHTDIV2
+        self.rect.x = Roulette.START_LEFT + Roulette.OFFSET*(n+1) + Card.WIDTH * n
+        self.rect.y = Game.HEIGHTDIV2/4 + Roulette.OFFSET*2
+        
+    def get_text_pos(self):
+        x = self.rect.center[0] - self.text_size[0]
+        y = self.rect.center[1] - self.text_size[1]
+        return (x,y)
         
     def _update(self):
-        self.rect.x += Card.STEP
-        if self.rect.x - Roulette.START_LEFT >= Roulette.ALL_WIDTH:
-            self.rect.x = Roulette.ALL_WIDTH - self.rect.x
+        self.rect.x += Roulette.STEP
+        if self.rect.x >= Roulette.ALL_WIDTH:
+            self.rect.x = self.rect.x  - Roulette.ALL_WIDTH
 
 def run():
     Color._set_all()
